d401fd62cffb7b526fa956fb8bd254bd0baf1599
sanyiam07 <89332850+sanyiam07@users.noreply.github.com>
2021-08-22 20:52:18 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+    $hostname ="http://localhost:8080/sanyiam/phppractice1/newsproject";
+    $conn = mysqli_connect("localhost","root","","news-site") or die("Connextion failed :".mysqli_connect_error());
+
+?>
